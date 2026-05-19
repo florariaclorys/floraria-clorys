dc1ef61278d1c6564c7f606368e51c304b3c905b
florariaclorys <florariaclorys@gmail.com>
2026-05-20 00:03:57 +0300

    Update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import HeroSection from '@/components/home/HeroSection'
-import FeaturesBar from '@/components/home/FeaturesBar'
 import CategoryGrid from '@/components/home/CategoryGrid'
 import FeaturedProducts from '@/components/home/FeaturedProducts'
 import TestimonialsSection from '@/components/home/TestimonialsSection'
@@ -8,7 +7,6 @@ export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <FeaturesBar />
       <CategoryGrid />
       <FeaturedProducts />
       <TestimonialsSection />
